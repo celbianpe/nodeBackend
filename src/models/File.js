9ed50b7f38b9b9c2ba0cb556fc4f7bd301530d0c
Celso Bianchi <celbianpe@outlook.com>
2019-04-21 01:24:36 -0300

    variaveis

--- src/models/File.js
+++ src/models/File.js
@@ -7,20 +7,17 @@ const File = new mongoose.Schema(
       required: true
     },
     path: {
-        type: String,
-        required: true
-      }
+      type: String,
+      required: true
+    }
   },
-  { timestamps: true,
-    toJSON : {virtuals:true},
-    toObject: {virtuals:true}
-}
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
-File.virtual('url').get(function(){
- return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+File.virtual("url").get(function() {
+  const url = process.env.URL || "http://localhost:3333";
 
-
-})
+  return `${url}/files/${encodeURIComponent(this.path)}`;
+});
 
 module.exports = mongoose.model("File", File);
